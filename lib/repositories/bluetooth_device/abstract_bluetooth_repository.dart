756a809d51df143a3f37f8958d12e2ba9a084a28
vladgrigorovich <vlad_grigorovich@icloud.com>
2023-04-07 18:22:47 +0300

    fix ble

--- lib/repositories/bluetooth_device/abstract_bluetooth_repository.dart
+++ lib/repositories/bluetooth_device/abstract_bluetooth_repository.dart
@@ -1,5 +1,8 @@
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:med_tech_mobile/repositories/bluetooth_device/bluetooth_device.dart';
 
 abstract class AbstractBluetoothRepository{
   Future<List<BluetoothDevice>> scanForDevices();
+  Future<void> stopScan();
+  Stream<DiscoveredDevice> _startScan();
 }
--- lib/repositories/bluetooth_device/bluetooth_device_repository.dart
+++ lib/repositories/bluetooth_device/bluetooth_device_repository.dart
@@ -1,40 +1,54 @@
+import 'dart:async';
+
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 import 'package:med_tech_mobile/repositories/bluetooth_device/bluetooth_device.dart';
 
-class BluetoothDeviceRepository implements AbstractBluetoothRepository{
+class BluetoothDeviceRepository implements AbstractBluetoothRepository {
 
   BluetoothDeviceRepository({required this.ble});
 
-  final FlutterReactiveBle ble;// = FlutterReactiveBle();
+  final FlutterReactiveBle ble; // = FlutterReactiveBle();
+  StreamSubscription? _scanSubscription; // Подписка на события сканирования
 
   @override
   Future<List<BluetoothDevice>> scanForDevices() async {
-
-    // UUID сервиса и характеристики, которые вам нужны для работы с BLE
-    const serviceUuid = 'your-service-uuid';
-    const characteristicUuid = 'your-characteristic-uuid';
-
-    final devices = ble.scanForDevices(
-      withServices: [Uuid.parse(serviceUuid)],
-      scanMode: ScanMode.lowLatency,
-    );
-
     final List<BluetoothDevice> bluetoothDevices = [];
 
-    await for (final device in devices) {
-      if (device.name.isNotEmpty) {
+    final scanResult = _startScan();
+
+    const scanDuration = Duration(seconds: 10); // Длительность сканирования
+    _scanSubscription = scanResult.take(scanDuration.inSeconds).listen((device) {
+      if (device.name.isNotEmpty) { // Если имя устройства не пустое, добавляем его в список
         final bluetoothDevice = BluetoothDevice(
+          //id: device.id,
           name: device.name,
         );
         bluetoothDevices.add(bluetoothDevice);
       }
-    }
-    // TODO: Верните список с полученными данными
+    });
+
+    await stopScan();
+
     return bluetoothDevices;
-   // throw UnimplementedError();
   }
 
-      //debugPrint('Found device: ${device.name} (${device.id})');
-      //debugPrint('Error occurred while scanning: $error');
+  Stream<DiscoveredDevice> _startScan() {
+    const scanMode = ScanMode.lowLatency; // Режим сканирования с низкой задержкой
+    const scanForAndroidOnlyServices = false;
+    return ble.scanForDevices(
+      withServices: [], // Сканирование всех доступных служб
+      scanMode: scanMode,
+      //requireLocationServicesEnabled: true, // Требование включенных служб геолокации.ХЗ надо или нет
+    );
+  }
 
-}
+  @override
+  Future<void> stopScan() async {
+    await _scanSubscription?.cancel();
+    _scanSubscription = null; // Обнуление подписки
+  }
+
+    //     //debugPrint('Found device: ${device.name} (${device.id})');
+    //     //debugPrint('Error occurred while scanning: $error');
+
+  }
