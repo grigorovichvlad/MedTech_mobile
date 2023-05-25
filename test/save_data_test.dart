fbbd2bc3bbe43a9219cf85dca6430f6a213aec26
vladgrigorovich <vlad_grigorovich@icloud.com>
2023-05-25 07:36:20 +0300

    Data tests

---
+++ test/save_data_test.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:med_tech_mobile/repositories/local_data_base/local_db_repository.dart';
+import 'package:med_tech_mobile/repositories/local_data_base/models/user_data.dart';
+
+void main() {
+  group('LocalDBRepository', () {
+    late LocalDBRepository localDBRepository;
+
+    setUp(() {
+      localDBRepository = LocalDBRepository();
+    });
+
+    test('Сохранение и чтение данных пользователя', () async {
+      final username = 'admin';
+      final password = 'password';
+      final token = 'token';
+
+      await localDBRepository.updateUserData(username, password, token);
+      final userData = await localDBRepository.readUsernamePassword();
+
+      expect(userData, equals([username, password]));
+    });
+
+    test('Удаление данных пользователя', () async {
+      final username = 'admin';
+      final password = 'password';
+      final token = 'token';
+
+      await localDBRepository.updateUserData(username, password, token);
+      await localDBRepository.deleteUserData();
+      final userData = await localDBRepository.readUsernamePassword();
+
+      expect(userData, equals([null, null]));
+    });
+
+    test('Добавление и удаление данных контроллера', () async {
+      final jsonData = '{"data": "controller data"}';
+
+      localDBRepository.addControllerData(jsonData);
+      final dataSize = await localDBRepository.getControllerDataSize();
+      final deletedData = await localDBRepository.getControllerDataSizeAndDelete();
+
+      expect(dataSize, equals(1));
+      expect(deletedData.length, equals(1));
+      expect(deletedData[0]?.dataJSON, equals(jsonData));
+    });
+  });
+}
